be834e0caba5fa7cfef160ed4c6f22cf767ae8ba
owner2plus.ai <owner2plus.ai@gmail.com>
2022-10-17 23:09:41 +0330

    vir 1.0

---
+++ image_fiter.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+import numpy as np
+#############################
+# a = [343,67]
+# b = [662,67]
+# c = [202,463]
+# d = [724,457]
+width = 500
+hight = 600
+#############################
+
+img = cv.imread("1.jpg")
+pts1 = np.float32([[343,67] ,[662,67] ,[292,463] ,[724,457]])
+pts2 = np.float32([[0,0],[width,0],[0, hight],[width,hight]])
+metrix = cv.getPerspectiveTransform(pts1,pts2)
+
+imgoutput = cv.warpPerspective(img, metrix, (width,hight))
+
+
+for x in range(0, 4):
+    cv.circle(img ,(int(pts1[x][0]) ,int(pts1[x][1])) ,5 ,(0,0,255) ,cv.FILLED)
+
+
+cv.imshow("result", img)
+cv.imshow("result", imgoutput)
+cv.waitKey(0)
+cv.destroyAllWindows()
